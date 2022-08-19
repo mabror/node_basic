c39e6a6c2434c85d2cc790321a0f1f3d8993e59d
mabror <mabror2001@gmail.com>
2022-08-19 11:58:14 +0500

    install node globals modules

---
+++ app.js
@@ -0,0 +1,15 @@
+// MODULES - Encapsulated Code (only share minimum)
+// common js, every file is module (by default)
+const names = require('./4-names')
+const sayHi = require('./5-utils')
+const data = require('./6-alternative-flavor')
+require('./7-mind-grenade')
+
+
+// console.log(names)
+
+
+
+// sayHi('susan')
+// sayHi(names.john)
+// sayHi(names.peter)
